623f17a3e25e478020feef07d61eb9ab559110a1
kimeshkov <mikekimbox@ya.ru>
2019-04-18 19:12:09 +0300

    add random questions

--- vk-bot-main/src/main/java/com/barinthecityshow/bot/dialog/chain/StickerDialogChain.java
+++ vk-bot-main/src/main/java/com/barinthecityshow/bot/dialog/chain/StickerDialogChain.java
@@ -2,28 +2,50 @@ package com.barinthecityshow.bot.dialog.chain;
 
 import com.barinthecityshow.bot.chain.ChainElement;
 import com.barinthecityshow.bot.dialog.QuestionAnswer;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 
-import java.util.Map;
+import java.util.List;
+import java.util.Random;
 
 public class StickerDialogChain implements DialogChain {
-    private static final Map<String, String> questionAns = ImmutableMap.<String, String>builder()
-            .put("Ты лох?", "Да")
-            .put("Ты нормальный?", "нет")
-            .put("Ты Ира?", "Да")
-            .build();
+    private static final List<QuestionAnswer> QUESTION_ANSWERS = initQuestionAnswers();
+
+    private final Random random;
+
+    public StickerDialogChain() {
+        random = new Random();
+    }
 
     @Override
     public ChainElement<QuestionAnswer> getFirst() {
-        QuestionAnswer questionAnswer = QuestionAnswer.builder()
-                .question("Сколько раз Бандерас выступал пьяным?")
-                .addCorrectAnswer("2")
-                .addCorrectAnswer("два")
-                .addCorrectAnswer("два раза")
-                .build();
-        return new QuestionAnswerChainElement(questionAnswer);
+        return randomQuestion();
     }
 
+    private ChainElement<QuestionAnswer> randomQuestion() {
+        return new QuestionAnswerChainElement(QUESTION_ANSWERS.get(random.nextInt(QUESTION_ANSWERS.size())));
+
+    }
+
+    private static List<QuestionAnswer> initQuestionAnswers() {
+        return Lists.newArrayList(
+                QuestionAnswer.builder()
+                        .question("Сколько раз Бандерас выступал пьяным?")
+                        .addCorrectAnswer("2")
+                        .addCorrectAnswer("два")
+                        .addCorrectAnswer("два раза")
+                        .addCorrectAnswer("дважды")
+                        .build(),
+                QuestionAnswer.builder()
+                        .question("Во сколько лет Нечаев сделал первую пародию?")
+                        .addCorrectAnswer("6")
+                        .addCorrectAnswer("шесть")
+                        .addCorrectAnswer("в шесть лет")
+                        .addCorrectAnswer("в шесть")
+                        .addCorrectAnswer("в 6 лет")
+                        .addCorrectAnswer("в 6")
+                        .build()
+        );
+
+    }
 
 }
